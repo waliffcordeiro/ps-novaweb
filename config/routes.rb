eac401020656b1717c4494bacf8badbdd02e702a
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2021-02-17 12:15:57 -0300

    Migrates e Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+
+      resources :contacts
+      resources :phones
+
+      # Add Phone Number
+      post 'contacts/:contact_id/phone', to: 'phones#create'
+
+    end
+  end
+
 end
